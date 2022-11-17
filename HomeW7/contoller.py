77bae4320b74add68ad70f1739ba53868ec4a378
MakcMay <makcmay@gmail.com>
2022-11-17 15:11:24 +0300

    add HW7

--- HomeW7/contoller.py
+++ HomeW7/contoller.py
@@ -1,36 +1,35 @@
 import view
 import logger
-import model_complex as mc
-import model_rational as mr
+import model
 
-def clac_start():
-    info = view.get_number_info()
-    if info == 1:
-        value_a = view.get_complex_value()
-        value_b = view.get_complex_value()
-        action = view.get_action()
-
-        mc.init(value_a, value_b, action)
-        result = mc.do_it()
-        view.view_data(result, value_a, value_b, action)
-        logger.write_log(info, value_a, value_b, action, result)
-
-    elif info == 2:
-        value_a = view.get_float_value()
-        value_b = view.get_float_value()
-        action = view.get_action()
-
-        mr.init(value_a, value_b, action)
-        result = mr.do_it()
-        view.view_data(result, value_a, value_b, action)
-        logger.write_log(info, value_a, value_b, action, result)
-
-    else:
-        print('ERR')
-
+def calc_start():
+    while True:
+        try:
+            info = view.get_number_info()
+            view.new_line()
+            if info == 1:
+                value_a = view.get_complex_value()
+                value_b = view.get_complex_value()
+                action = view.get_action()
+            elif info == 2:
+                value_a = view.get_float_value()
+                value_b = view.get_float_value()
+                action = view.get_action()
+            elif info == 3:
+                break
+            else:
+                view.menu_error()
+                continue
+            view.new_line()
+            model.init(value_a, value_b, action)
+            result = model.do_it()
+            view.view_data(result, value_a, value_b, action)
+            logger.write_log(info, value_a, value_b, action, result)
+            view.new_line()
+        except:
+            view.menu_error()
+            view.new_line()
     
-    
-
 
 
 
--- HomeW7/view.py
+++ HomeW7/view.py
@@ -1,17 +1,64 @@
+from decimal import Decimal
+
 def view_data(data,value_a, value_b, action):
-    print(f'{value_a}{action}{value_b} = {data}')
+    print(f'Результат {value_a}{action}{value_b} = {data}')
 
 def get_float_value():
-    return float(input('value = '))
+    '''
+    Пользователь вводит число
+    '''
+    flag = False
+    while not flag:
+        try:
+            number = float(input("Число = "))
+            flag = True
+        except ValueError:
+            print("Ошибка, попробуйте еще раз!")
+    return number
 
-def get_complex_value():
-    return complex(input('value = '))
+# def get_complex_value():
+#     '''Пользователь вводит комплексное число
+#     в формате a+bj, можно ввести только (a) тогда формат a+0j'''
+#     return complex(input('value = '))
 
 def get_action():
+    '''Пользователь вводит действие +-*/'''
     return str(input('Введите действие +-*/  = '))
 
-def get_number_info():
-    return int(input('Введите тип данных, комплексные = 1, рациональные = 2 : '))
+# def get_number_info():
+#     '''Выбор типа данных для работы,
+#     комплексные = 1, рациональные = 2'''
+#     return int(input('Введите тип данных, комплексные = 1, рациональные = 2 : '))
 
 mode = {'+': 'сумма', '-': 'разность', '*': 'произведение', '/': 'деление'}
 info = {1: 'комплексные', 2: 'рациональные'}
+
+def get_complex_value():
+    flag = False
+    while not flag:
+        try:
+            number = complex(input("Число = "))
+            flag = True
+        except ValueError:
+            print("Ошибка, попробуйте еще раз!")
+    return number
+
+def get_number_info():
+    flag = False
+    while not flag:
+        try:
+            number = int(input('Введите тип данных:\n'
+                     'Комплексные = 1\n'
+                     'Рациональные = 2\n'
+                     'Выход из программы = 3: '))
+            if 0 < number < 4:
+                flag = True
+        except ValueError:
+            print("Ошибка, попробуйте еще раз!")
+    return number
+
+def new_line():
+    print()
+
+def menu_error():
+    print('Ошибка выбора меню')        
